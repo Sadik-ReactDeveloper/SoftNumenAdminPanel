19fcf8e510cb000162ae1c5397b2b2754f4d327d
prandeveloper <pranaydevelopersveltosest@gmail.com>
2023-10-10 17:29:24 +0530

    build

--- src/views/apps/freshlist/parts/PartCatalougue.js
+++ src/views/apps/freshlist/parts/PartCatalougue.js
@@ -1,77 +1,3 @@
-// import React from "react";
-// import { Collapse } from "reactstrap";
-
-// const collapseItems = [
-//   {
-//     id: 1,
-//     title: "Accordion Item 1",
-//     content:
-//       "Pie dragée muffin. Donut cake liquorice marzipan carrot cake topping powder candy. Sugar plum brownie brownie cotton candy.",
-//   },
-//   {
-//     id: 2,
-//     title: "Accordion Item 2",
-//     content:
-//       "Jelly-o brownie marshmallow soufflé I love jelly beans oat cake. I love gummies chocolate bar marshmallow sugar plum.",
-//   },
-//   {
-//     id: 3,
-//     title: "Accordion Item 3",
-//     content:
-//       "Pudding lollipop dessert chocolate gingerbread. Cake cupcake bonbon cupcake marshmallow. Gummi bears carrot cake bonbon cake.",
-//   },
-//   {
-//     id: 4,
-//     title: "Accordion Item 4",
-//     content:
-//       "Brownie sweet carrot cake dragée caramels fruitcake. Gummi bears tootsie roll croissant gingerbread dragée tootsie roll.",
-//   },
-// ];
-
-// class AccordionBorder extends React.Component {
-//   state = { collapseID: "" };
-
-//   toggleCollapse = (collapseID) => {
-//     this.setState((prevState) => ({
-//       collapseID: prevState.collapseID !== collapseID ? collapseID : "",
-//     }));
-//   };
-
-//   render() {
-//     const accordionBorderItems = collapseItems.map((collapseItem) => {
-//       return (
-//         <Card
-//           className="collapse-border-item"
-//           key={collapseItem.id}
-//           onClick={() => this.toggleCollapse(collapseItem.id)}
-//         >
-//           <CardHeader>
-//             <CardTitle className="lead collapse-title collapsed">
-//               {collapseItem.title}
-//             </CardTitle>
-//           </CardHeader>
-//           <Collapse
-//             isOpen={collapseItem.id === this.state.collapseID}
-//             onEntering={this.onEntering}
-//             onEntered={this.onEntered}
-//             onExiting={this.onExiting}
-//             onExited={this.onExited}
-//           >
-//             <CardBody>{collapseItem.content}</CardBody>
-//           </Collapse>
-//         </Card>
-//       );
-//     });
-
-//     return (
-//       <div className="collapse-bordered vx-collapse collapse-icon accordion-icon-rotate collapse-border">
-//         {accordionBorderItems}
-//       </div>
-//     );
-//   }
-// }
-// export default AccordionBorder;
-
 import React, { useState, useEffect } from "react";
 import Imagemagnify from "./Imagemagnify";
 import {
@@ -88,7 +14,7 @@ import "./Magnify.css";
 import { AiFillDownCircle, AiFillUpCircle } from "react-icons/ai";
 import { PartCatalogueView } from "../../../../ApiEndPoint/ApiCalling";
 
-function AccordionBorder() {
+function PartCatalougue() {
   const [CollapseIndex, setCollapseIndex] = useState("");
   const [frontSide, setfrontSide] = useState([]);
   const [AllData, setAllData] = useState({});
@@ -173,19 +99,13 @@ function AccordionBorder() {
         <Col lg="5" md="5" sm="5" xs="12">
           {ListData && ListData ? (
             <>
-              {" "}
               <Imagemagnify imageSrc={ListData[0]?.Part_Image?.text} />
             </>
           ) : (
             <>
-              {" "}
               <Imagemagnify imageSrc="https://res.cloudinary.com/dc7hzwpbm/image/upload/v1683461876/software_development.jpg" />
             </>
           )}
-
-          {/* <div className="collapse-bordered vx-collapse collapse-icon accordion-icon-rotate collapse-border">
-            {accordionBorderItems}
-          </div> */}
         </Col>
         <Col lg="4" md="4" sm="4" xs="12">
           <div
@@ -196,10 +116,6 @@ function AccordionBorder() {
               <thead>
                 <tr>
                   <th>#</th>
-                  {/* <th>_id</th>
-                    <th>Parts_Catalogue</th>
-                    <th>Part_Catalogue</th> */}
-                  {/* <th>Part Image</th> */}
                   <th>Part Name</th>
                   <th>Part Number</th>
                   <th>Part Quantity</th>
@@ -230,4 +146,4 @@ function AccordionBorder() {
   );
 }
 
-export default AccordionBorder;
+export default PartCatalougue;
