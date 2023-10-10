df1a3109292184f370b5b5a4f89873ecd65b6d27
anujeshyadav <anujeshdevelopersveltosest11@gmail.com>
2023-10-10 17:30:02 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Router from "./Router";
 import "./components/@vuexy/rippleButton/RippleButton";
-import "bootstrap/dist/css/bootstrap.css";
 
 import "react-perfect-scrollbar/dist/css/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
